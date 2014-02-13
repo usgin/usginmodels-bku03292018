31486cb57886e3ed3d9f803b9f97863bc50c38ac
asonnenschein <adrian.sonnenschein@gmail.com>
2014-02-12 22:38:09 -0700

    setup .cfg .py files for pip

---
+++ setup.py
@@ -0,0 +1,16 @@
+# usginmodels' setup.py
+
+from distutils.core import setup
+
+setup(
+    name = 'usginmodels',
+    packages = ['usginmodels'],
+    version = '1.0.0',
+    description = 'Interface for interacting with USGIN content models',
+    author = 'Arizona Geological Survey',
+    author_email = 'adrian.sonnenschein@gmail.com',
+    url = 'https://github.com/usgin/usginmodels',
+    download_url = 'https://github.com/usgin/usginmodels/tarball/1.0.0',
+    keywords = ['usgin', 'ngds', 'azgs'],
+    classifiers = [],
+)
