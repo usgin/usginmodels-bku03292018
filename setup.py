c84bf4328e68c7fc4718b8d528a6fa25424a2418
JessicaA <jalisdairi@gmail.com>
2014-04-09 15:34:33 -0700

    updated setup.py with 1.1.4 tag

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
     name = 'usginmodels',
     packages = ['usginmodels'],
-    version = '1.1.3',
+    version = '1.1.4',
     description = 'Interface for interacting with USGIN content models',
     author = 'Arizona Geological Survey',
     author_email = 'adrian.sonnenschein@gmail.com',
     url = 'https://github.com/usgin/usginmodels',
-    download_url = 'https://github.com/usgin/usginmodels/tarball/1.1.3',
+    download_url = 'https://github.com/usgin/usginmodels/tarball/1.1.4',
     keywords = ['usgin', 'ngds', 'azgs'],
     classifiers = [],
 )
